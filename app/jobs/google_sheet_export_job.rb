6402b2d1bb62a0687bbe283ec8b035be964ffd9b
Justin Sabelko <justin.sabelko@cru.org>
2024-12-18 13:58:49 -0600

    Add job files

---
+++ app/jobs/google_sheet_export_job.rb
@@ -0,0 +1,7 @@
+class GoogleSheetExportJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ app/jobs/salesforce_export_job.rb
@@ -0,0 +1,7 @@
+class SalesforceExportJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
