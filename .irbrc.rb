b631904c5fb1fc297864baf4ba79fa74ec841ce7
Justin Sabelko <justin.sabelko@cru.org>
2025-01-22 14:15:12 -0600

    Prompt and standard fixes

--- .irbrc.rb
+++ .irbrc.rb
@@ -1,16 +1,16 @@
 require "pry"
-Pry.config.prompt = [
-  proc do
-    current_app = ENV["MARCO_POLO_APP_NAME"] || Rails.application.class.module_parent_name.underscore.tr("_", "-")
-    rails_env = Rails.env.downcase
+Pry.config.prompt = Pry::Prompt.new(
+  "custom", "my custom prompt", [
+    proc do |obj, nest_level, _|
+      current_app = ENV["MARCO_POLO_APP_NAME"] || Rails.application.class.module_parent_name.underscore.tr("_", "-")
+      rails_env = Rails.env.downcase
 
-    # shorten some common long environment names
-    rails_env = "dev" if rails_env == "development"
-    rails_env = "prod" if rails_env == "production"
-
-    "#{current_app}-pry(#{rails_env})> "
-  end,
-  proc { "> " }
-]
+      # shorten some common long environment names
+      rails_env = "dev" if rails_env == "development"
+      rails_env = "prod" if rails_env == "production"
+      "#{current_app}-pry(#{rails_env}):#{nest_level}> "
+    end
+  ]
+)
 Pry.start
 exit
