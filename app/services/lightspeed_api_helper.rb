e164838e2500ad2545dea5486a01e10df20dfe99
Jon Watson <jon.watson@cru.org>
2025-02-03 16:37:53 -0700

    One more fix to price rounding.

--- app/services/lightspeed_api_helper.rb
+++ app/services/lightspeed_api_helper.rb
@@ -240,7 +240,7 @@ class LightspeedApiHelper
     end
     if total != subtotal
       difference = subtotal - total
-      prices[-1] += difference.to_f.round(2)
+      prices[-1] += '%.2f' % difference
     end
     prices
   end
@@ -260,8 +260,8 @@ class LightspeedApiHelper
       end
     end
     if total != tax_total
-      difference = tax_total - total
-      taxes[-1] += difference.to_f.round(2)
+      difference = '%.2f' % (tax_total - total)
+      taxes[-1] += difference
     end
     taxes
   end
