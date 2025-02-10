064a94f33e7c5f583fd90ff4975fada210abca14
Justin Sabelko <justin.sabelko@cru.org>
2025-02-10 11:15:17 -0600

    Fix spec

--- app/services/lightspeed_token_holder.rb
+++ app/services/lightspeed_token_holder.rb
@@ -1,25 +1,10 @@
 require "httparty"
-require "dotenv"
-# Load .env.local instead of .env
-# Dotenv.overload(".env.local")
 
 class LightspeedTokenHolder
-  @token = nil
-  @refresh_token = nil
-  @expires_in = nil
-  @token_type = nil
-
   attr_reader :token
-
   attr_reader :refresh_token
-
   attr_reader :expires_in
-
   attr_reader :token_type
-
-  LIGHTSPEED_API_AUTH_ROOT = ENV["LIGHTSPEED_API_AUTH_ROOT"]
-  LIGHTSPEED_CLIENT_ID = ENV["LIGHTSPEED_CLIENT_ID"]
-  LIGHTSPEED_CLIENT_SECRET = ENV["LIGHTSPEED_CLIENT_SECRET"]
 
   def initialize(token: nil, refresh_token: nil)
     raw = Rails.cache.read("lightspeed_auth")
@@ -36,11 +21,11 @@ class LightspeedTokenHolder
 
   def trade_access_token(code)
     Rails.logger.info "Getting Lightspeed API token..."
-    url = LIGHTSPEED_API_AUTH_ROOT.dup
+    url = ENV["LIGHTSPEED_API_AUTH_ROOT"]
     # use httparty to make a post request to the lightspeed auth endpoint
     response = HTTParty.post(url, body: {
-      client_id: LIGHTSPEED_CLIENT_ID,
-      client_secret: LIGHTSPEED_CLIENT_SECRET,
+      client_id: ENV["LIGHTSPEED_CLIENT_ID"],
+      client_secret: ENV["LIGHTSPEED_CLIENT_SECRET"],
       grant_type: "authorization_code",
       code: code
     })
@@ -60,9 +45,9 @@ class LightspeedTokenHolder
 
   def refresh_oauth_token
     Rails.logger.info "Refreshing Lightspeed API token..."
-    url = LIGHTSPEED_API_AUTH_ROOT.dup
+    url = ENV["LIGHTSPEED_API_AUTH_ROOT"]
     # use httparty to make a post request to the lightspeed auth endpoint
-    response = HTTParty.post(url, body: {client_id: LIGHTSPEED_CLIENT_ID, client_secret: LIGHTSPEED_CLIENT_SECRET, grant_type: "refresh_token", refresh_token: @refresh_token})
+    response = HTTParty.post(url, body: {client_id: ENV["LIGHTSPEED_CLIENT_ID"], client_secret: ENV["LIGHTSPEED_CLIENT_SECRET"], grant_type: "refresh_token", refresh_token: @refresh_token})
     if response.code == 200
       Rails.logger.info "Token refreshed successfully - #{response.body}"
       # write the refreshed response to cache
