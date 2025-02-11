42020b34e248c7eed2e4092669b91b8db2d648de
Justin Sabelko <justin.sabelko@cru.org>
2025-02-11 15:46:05 -0600

    Allow WooImport jobs to retry

--- app/jobs/woocommerce_import_job.rb
+++ app/jobs/woocommerce_import_job.rb
@@ -1,7 +1,11 @@
 class WoocommerceImportJob < ApplicationJob
   queue_as :default
+  sidekiq_options retry: true
 
   def perform(*args)
     WooImport.new.poll_jobs
+  rescue StandardError => e
+    WooImport.where(type: "WOO_IMPORT", status: :processing).order(updated_at: :desc).first.status_error!
+    raise e
   end
 end
