8ea654c885a6cd78f22cd57d3b1df8b21ed599d3
Justin Sabelko <justin.sabelko@cru.org>
2025-02-17 15:48:31 -0600

    Specs

--- spec/jobs/poll_sheet_job_spec.rb
+++ spec/jobs/poll_sheet_job_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe PollSheetJob, type: :job do
     expect_any_instance_of(PollSheet).to receive(:poll_jobs)
     expect { PollSheetJob.perform_now }.to change { Job.count }.by(1)
   end
+
+  it "rescues and re-raises exceptions" do
+    job = create(:job, type: "POLL_SHEET", status: :processing)
+    LightspeedStubHelpers.stub_lightspeed_account_request
+    allow_any_instance_of(PollSheet).to receive(:poll_jobs).and_raise("An error occurred")
+    expect {
+      PollSheetJob.perform_now
+    }.to raise_error("An error occurred")
+    expect(job.reload.status).to eq("error")
+  end
 end
--- spec/jobs/woocommerce_refresh_job_spec.rb
+++ spec/jobs/woocommerce_refresh_job_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe WoocommerceRefreshJob, type: :job do
     expect_any_instance_of(WooRefresh).to receive(:poll_jobs)
     expect { WoocommerceRefreshJob.perform_now }.to change { Job.count }.by(1)
   end
+
+  it "rescues and re-raises exceptions" do
+    job = create(:job, type: "WOO_REFRESH", status: :processing)
+    LightspeedStubHelpers.stub_lightspeed_account_request
+    allow_any_instance_of(WooRefresh).to receive(:poll_jobs).and_raise("An error occurred")
+    expect {
+      WoocommerceRefreshJob.perform_now
+    }.to raise_error("An error occurred")
+    expect(job.reload.status).to eq("error")
+  end
 end
