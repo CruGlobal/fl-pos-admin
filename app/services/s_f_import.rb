d54ad0ad0b6df06ab5e2e413a94ee95505a53e7f
Justin Sabelko <justin.sabelko@cru.org>
2025-10-28 12:28:32 -0500

    Pass in API_VERSION to Salesforce config

--- app/services/s_f_import.rb
+++ app/services/s_f_import.rb
@@ -16,7 +16,8 @@ class SFImport
       instance_url: ENV["SF_INSTANCE_URL"],
       host: ENV["SF_HOST"],
       client_id: ENV["SF_CLIENT_ID"],
-      client_secret: ENV["SF_CLIENT_SECRET"]
+      client_secret: ENV["SF_CLIENT_SECRET"],
+      api_version: ENV["SF_VERSION"]
     )
   end
 
